79e0957f3afdbd8d4a245fc5858ebbb0e5ca232c
impothnis <pavanpothnis@outlook.com>
2021-06-06 11:08:58 +0530

    Printing Styles

---
+++ APCS/src/apcs/PrintingStyles.java
@@ -0,0 +1,11 @@
+package apcs;
+
+public class PrintingStyles {
+
+	public static void main(String args[]) 
+    {
+        System.out.println("My name is Pavan and I am 31 years old.");
+        System.out.println("I am learning Java.");
+    }
+	
+}
